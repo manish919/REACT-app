3a0bacb195aa9e923e10533258b416f744cea8a8
Manish Ale Magar <manish.magar919@gmail.com>
2021-04-04 14:55:16 -0500

    Completed part 2.11

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-function App() {
+const App = () => {
+  const [ persons, setPersons ] = useState([]) 
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  const [ searchName, setSearchName ] = useState('')
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+    .then(response => {
+      setPersons(response.data)
+    })
+  }, [])
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newPerson = { name: newName, number: newNumber};
+    const searchArray = persons.filter(person => person.name.toLowerCase() === newName.toLowerCase())
+    if(searchArray.length > 0)
+      alert(`${newName} is already added to phonebook`);
+    else{
+      setPersons(persons.concat(newPerson));
+    }
+      
+      setNewName('');
+      setNewNumber('');
+  }
+
+  const handleNameChange = (event) => {
+    // console.log(event.target.value);
+    setNewName(event.target.value);
+  }
+
+  const handleNumberChange = (event) => {
+    // console.log(event.target.value);
+    setNewNumber(event.target.value);
+  }
+
+  const handleSearchChange = (event) => {
+    setSearchName(event.target.value);
+  }
+
+  const filteredPersons = persons.filter(person => {
+    return person.name.toLowerCase().includes(searchName)
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={searchName} onChange={handleSearchChange} />
+      </div>
+      <h2>add a new</h2>
+      <form onSubmit={handleSubmit}> 
+        
+        <div>
+          name: <input onChange={handleNameChange} value={newName}/>
+        </div>
+        <div>
+          number: <input onChange={handleNumberChange} value={newNumber}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {
+        filteredPersons.map(person => <div key={person.name}>{person.name} {person.number}</div>)
+      }
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Note.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Note = (props) => {
+
+    const {note} = props;
+    return(
+        <li>{note.content}</li>
+    );
+}
+
+export default Note;
